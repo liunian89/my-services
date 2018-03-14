8e8cd5f42247d28168f0f3404d52f6cb8f302f0c
Nian Liu <nliu@bol.com>
2018-03-14 23:48:42 +0100

    set up spring

---
+++ wechat/src/main/java/com/liunian/config/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.liunian.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+
+}
---
+++ wechat/src/main/java/com/liunian/endpoints/MasterController.java
@@ -0,0 +1,14 @@
+package com.liunian.endpoints;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MasterController {
+
+    @GetMapping("/")
+    public String root() {
+        return "Hello Wechat!";
+    }
+
+}
