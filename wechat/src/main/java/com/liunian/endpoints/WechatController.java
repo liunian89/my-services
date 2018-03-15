df35972d8e5c819bd03a3e456d14561330e6ea92
Nian Liu <nliu@bol.com>
2018-03-15 22:17:48 +0100

    token

--- wechat/src/main/java/com/liunian/endpoints/WechatController.java
+++ wechat/src/main/java/com/liunian/endpoints/WechatController.java
@@ -9,6 +9,8 @@ import java.util.Arrays;
 
 @RestController
 public class WechatController {
+    private static final String TOKEN = "7_ixt7Mne7k9YL9DOmovsnguOoC-i-5rTd2QkqEpvc-rrsvQwa7RWpMS8Mwmz1fqom2ss1Z55w4cmvHdgvj7MAu1LlrJqRIazAdfdWVWDeY8oaJR7nzy2ZLnQPlQz5R7xNfwm1YU26aMQE3tzyYKGgAEAOAW";
+
 
     // Verify wechat - server communication
     @RequestMapping(value = "/wechat", method = RequestMethod.GET)
@@ -40,7 +42,7 @@ public class WechatController {
     }
 
     private boolean isValid(String signature, String timestamp, String nonce) {
-        String[] stringList = {timestamp, nonce, "7_ecjAzWq89Int9AGw9FlK5SPBE7BwwoQuU-eoJ4-jZfQmFg4wBuJCRnk42XtIKSxCx5rfBURN7g0PMWzOCFTcez7ekfgfZyPsyAWZR887S4YZvGHmz6KAzfXtvt9TvnvzH_Nbp00l7GCcQJg2NCGfADATIM"};
+        String[] stringList = {timestamp, nonce, TOKEN};
         Arrays.sort(stringList);
         String join = org.apache.commons.lang.StringUtils.join(stringList);
         String encoded = DigestUtils.sha1Hex(join);
