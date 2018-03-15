bc51d46126e579346d6fb5ae42345a6170c525af
Nian Liu <nliu@bol.com>
2018-03-15 21:06:16 +0100

    endpoint for receiving message

--- wechat/src/main/java/com/liunian/endpoints/WechatController.java
+++ wechat/src/main/java/com/liunian/endpoints/WechatController.java
@@ -10,24 +10,12 @@ import java.util.Arrays;
 @RestController
 public class WechatController {
 
-    @GetMapping("/")
-    public String root() {
-        return "Hello Wechat!";
-    }
-
-    /**
-     * 验证微信服务器
-     *
-     * @param signature
-     * @param timestamp
-     * @param nonce
-     * @param echostr
-     */
-    @RequestMapping(value = "/wechat1", method = RequestMethod.GET)
-    public String verifyToken(@RequestParam String signature,
-                              @RequestParam String timestamp,
-                              @RequestParam String nonce,
-                              @RequestParam String echostr) {
+    // Verify wechat - server communication
+    @RequestMapping(value = "/wechat", method = RequestMethod.GET)
+    public String serverValidation(@RequestParam String signature,
+                                   @RequestParam String timestamp,
+                                   @RequestParam String nonce,
+                                   @RequestParam String echostr) {
         System.out.println(String.format("[%s] [%s] [%s] [%s]", signature, timestamp, nonce, echostr));
 
         if (isValid(signature, timestamp, nonce)) {
@@ -36,22 +24,14 @@ public class WechatController {
         return null;
     }
 
-    /**
-     * 验证微信服务器
-     *
-     * @param response
-     * @param signature
-     * @param timestamp
-     * @param nonce
-     * @param echostr
-     */
+    // Verify wechat - server communication
     @RequestMapping(value = "/wechat2", method = RequestMethod.GET)
-    public void verifyToken(PrintWriter out,
-                            HttpServletResponse response,
-                            @RequestParam String signature,
-                            @RequestParam String timestamp,
-                            @RequestParam String nonce,
-                            @RequestParam String echostr) {
+    public void serverValidation(PrintWriter out,
+                                 HttpServletResponse response,
+                                 @RequestParam String signature,
+                                 @RequestParam String timestamp,
+                                 @RequestParam String nonce,
+                                 @RequestParam String echostr) {
         System.out.println(String.format("[%s] [%s] [%s] [%s]", signature, timestamp, nonce, echostr));
 
         if (isValid(signature, timestamp, nonce)) {
@@ -65,6 +45,11 @@ public class WechatController {
         String join = org.apache.commons.lang.StringUtils.join(stringList);
         String encoded = DigestUtils.sha1Hex(join);
         return signature.equals(encoded);
+    }
+
+    @PostMapping("/wechat")
+    public void onMessage(@RequestBody String message) {
+        System.out.println(message);
     }
 
 }
